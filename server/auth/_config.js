aeb616fc7be8e814ad83cc0dfac6682afac19900
sodas43 <souvikd.pro@gmail.com>
2018-04-17 12:25:40 +0530

    Update _config.js
    updated twitter and google callback url

--- server/auth/_config.js
+++ server/auth/_config.js
@@ -8,13 +8,13 @@ var ids = {
 	'twitter' : {
 		'consumerKey' 		: 'xaHgDlC54cC6DrNBkTzaObi1f',
 		'consumerSecret' 	: 'GKpcQTaEteVRU1MkVcvhPEuO1lwKBQw7uVy6xI63UC2T1tTW0C',
-		'callbackURL' 		: 'http://127.0.0.1:4200/auth/twitter/callback'
+		'callbackURL' 		: 'https://mutualcap1.herokuapp.com/auth/twitter/callback'
 	},
 
 	'google' : {
 		'clientID' 	: '662289305999-qudd5ebc52l79n9fmafpej2nd2rp22c0.apps.googleusercontent.com',
 		'clientSecret' 	: 'UpUwGXgU4cUW6LVSdcmdfZGB',
-		'callbackURL' 	: 'http://127.0.0.1:4200/auth/google/callback'
+		'callbackURL' 	: 'https://mutualcap1.herokuapp.com/auth/google/callback'
 	},
 	// 'sendgrid' : {
 	// 	'API_KEY' : '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'
